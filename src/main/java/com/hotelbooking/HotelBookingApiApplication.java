7de1ac4422848f337b62cce5b950a81a6d7195a6
Josefin Vingeskog <josefin.vingeskog@hotmail.com>
2026-02-13 12:00:39 +0100

    Initial Spring Boot setup with layered structure and base security config

---
+++ src/main/java/com/hotelbooking/HotelBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.hotelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotel_booking_api/HotelBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotel_booking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
